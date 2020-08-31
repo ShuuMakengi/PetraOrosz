77cda0678d09aa7eaf53cdc60ceace4c7b62157a
ShuuMakengi <oroszpetra11@gmail.com>
2020-08-31 21:56:31 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PetraPort';
+  title = 'Petra Orosz';
+  email = 'oroszpetra11@gmail.com';
+  phone = '+36 30 2958 122';
 }
